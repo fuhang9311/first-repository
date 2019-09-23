e65a17bb778afd26d1ffd2ac8903613065c4fbcd
fuhang9311 <45188235+fuhang9311@users.noreply.github.com>
2019-09-23 16:50:24 +0800

    增加get、set方法

--- src/test/java/com/fuhang/reflect/Person.java
+++ src/test/java/com/fuhang/reflect/Person.java
@@ -2,4 +2,12 @@ package com.fuhang.reflect;
 
 public class Person {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
